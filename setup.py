56c9a78fed45adde848325defeffe4a2bbac70d6
Jens W. Klein <jens@bluedynamics.com>
2010-12-28 18:58:13 +0100

    prepare a release

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-# Copyright 2009, BlueDynamics Alliance - http://bluedynamics.com
-# Python Software Foundation License
-
 from setuptools import setup, find_packages
-import sys, os
+import os
 
-version = '1.0'
-shortdesc = 'YAFOWIL: Integration with WebOb'
+version = '1.0-beta'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
+shortdesc = 'YAFOWIL - Yet Another Form Widget Lib: Integration with WebOb'
 tests_require = ['interlude']
 
 setup(name='yafowil.webob',
@@ -14,19 +13,21 @@ setup(name='yafowil.webob',
       description=shortdesc,
       long_description=longdesc,
       classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'License :: OSI Approved :: Python Software Foundation License',
+            #'Development Status :: 5 - Production/Stable',
+            'Development Status :: 4 - Beta',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Topic :: Software Development',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'License :: OSI Approved :: BSD License',                        
       ],
-      keywords='webob request response html input widgets',
-      author='Jens W. Klein',
+      keywords='zope2 request response html input widgets',
+      author='BlueDynamics Alliance',
       author_email='dev@bluedynamics.com',
-      url=u'',
-      license='Python Software Foundation License',
+      url=u'http://packages.python.org/yafowil.webob',
+      license='Simplified BSD',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['yafowil'],
       include_package_data=True,
       zip_safe=True,
@@ -35,9 +36,9 @@ setup(name='yafowil.webob',
           'WebOb',
           'yafowil',
       ],
-      tests_require=tests_require,
-      test_suite="yafowil.webob.tests.test_suite",
-      extras_require = dict(
-          tests=tests_require,
-      ),
+      #tests_require=tests_require,
+      #test_suite="yafowil.zope2.tests.test_suite",
+      #extras_require = dict(
+      #    tests=tests_require,
+      #),
 )
