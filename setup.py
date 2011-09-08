a1b19d5ede019affbced6c146c67d3650bfb0c32
Jens W. Klein <jens@bluedynamics.com>
2011-09-08 13:47:50 +0200

    make 1.0 release to fix the version confusion on pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.9'
+version = '1.0'
 shortdesc = 'YAFOWIL - Yet Another Form Widget Lib: Integration with WebOb'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
